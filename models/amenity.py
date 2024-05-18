0c63f4331dd6814fae4119c445cdc4e0a58a31f9
“QwesiAlonso” <sammyalonso4@gmail.com>
2024-05-18 14:29:27 -0700

    updated

---
+++ models/amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This Defines the Amenity class.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This Represent an amenity.
+
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
